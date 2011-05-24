04e9c84ef25cd81ea05d09a93c55792ac19ee89c
Neth Shady <Neth.Shady@web.de>
2011-05-24 16:51:55 +0200

    Added a Test Rule text file, the Parser correctly checks it for correctness. Currently, only a hash is returned that tells how many Areas and Rooms were parsed and in which order.

---
+++ src/jworldgen/filehandler/TextFileReader.java
@@ -0,0 +1,42 @@
+package jworldgen.filehandler;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TextFileReader {
+
+	public static String readTextFile(String filename) {
+		File file = new File(filename);
+		StringBuffer contents = new StringBuffer();
+	    BufferedReader reader = null;
+
+	    try {
+	    	reader = new BufferedReader(new FileReader(file));
+	        String text = null;
+
+	        // repeat until all lines is read
+	        while ((text = reader.readLine()) != null)
+	        	contents.append(text).append(System.getProperty("line.separator"));
+
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                if (reader != null) {
+	                    reader.close();
+	                }
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        // show file contents here
+	        return contents.toString();
+	    }
+	}
+
+
