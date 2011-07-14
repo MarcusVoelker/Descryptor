4aebc6e46e11b204f7c23b7314c4feac44673f54
Neth Shady <Neth.Shady@web.de>
2011-07-14 19:27:55 +0200

    Improved the 3D-Viewer with Key-controlled rotation and a reset key
    Change-Id: Ia675d16589f698fe629d60f371c274e1d135125c

--- src/jworldgen/test/TestStartup3D.java
+++ src/jworldgen/test/TestStartup3D.java
@@ -17,7 +17,7 @@ import org.lwjgl.util.glu.GLU;
 
 public class TestStartup3D {
 	
-	
+	private static final float ROT_SPEED = 3;
 	private static int cuttingDepth = 0;
 	private static boolean pressed = false;
 	private static void setColor(int hue, float sat, float val, int max)
@@ -59,62 +59,82 @@ public class TestStartup3D {
 	    }
 		return false;
 	}
+	static World world;
+	static byte[][][] drawThis;
+	
+	private static void setDrawingStates()
+	{
+		for (int i = cuttingDepth; i < world.getWidth() - cuttingDepth; i++) 
+		{
+			for (int j = cuttingDepth; j < world.getHeight() - cuttingDepth; j++) 
+			{
+				for (int k = cuttingDepth; k < world.getDepth() - cuttingDepth; k++) 
+				{
+					drawThis[i][j][k] = (byte) (world.getValue(i, j, k) == 0 ? 0 : 1);
+				}
+			}
+		}
+		for (int i = cuttingDepth + 1; i < world.getWidth()-1 - cuttingDepth; i++) 
+		{
+			for (int j = cuttingDepth + 1; j < world.getHeight()-1 - cuttingDepth; j++) 
+			{
+				for (int k = cuttingDepth + 1; k < world.getDepth()-1 - cuttingDepth; k++) 
+				{
+					if (drawThis[i][j][k] == 1)
+					{
+						drawThis[i][j][k] = 2;
+						if (drawThis[i+1][j][k] == 0)
+							drawThis[i][j][k] = 1;
+						else if (drawThis[i-1][j][k] == 0)
+							drawThis[i][j][k] = 1;
+						else if (drawThis[i][j+1][k] == 0)
+							drawThis[i][j][k] = 1;
+						else if (drawThis[i][j-1][k] == 0)
+							drawThis[i][j][k] = 1;
+						else if (drawThis[i][j][k+1] == 0)
+							drawThis[i][j][k] = 1;
+						else if (drawThis[i][j][k-1] == 0)
+							drawThis[i][j][k] = 1;
+					}
+				}
+			}
+		}
+	}
+	private static void load(String filename)
+	{
+		try {
+			world = Generator.generateFromFile(filename, 50, 50, 50);
+			drawThis = new byte[world.getWidth()][world.getHeight()][world.getDepth()];
+			setDrawingStates();
+		} catch (CriticalFailure e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args)
 	{
 		try {
 			Display.setDisplayMode(new DisplayMode(800,800));
 			Display.create();
-			World world = Generator.generateFromFile("data/WorleyRules.txt", 50, 50, 50);
+			load("data/WorleyRules.txt");
 			GL11.glMatrixMode(GL11.GL_PROJECTION);
 			GL11.glLoadIdentity();
 			GLU.gluPerspective(45.0f, 1.0f, 0.5f, 400.0f);
 			GL11.glMatrixMode(GL11.GL_MODELVIEW);
 			glEnable(GL_DEPTH_TEST);
 			GL11.glCullFace(GL11.GL_FRONT); // Doesn't draw back faces
-			float angle = 0.01f;
-			byte[][][] drawThis = new byte[world.getWidth()][world.getHeight()][world.getDepth()];
-			for (int i = 0; i < world.getWidth(); i++) 
-			{
-				for (int j = 0; j < world.getHeight(); j++) 
-				{
-					for (int k = 0; k < world.getDepth(); k++) 
-					{
-						drawThis[i][j][k] = (byte) (world.getValue(i, j, k) == 0 ? 0 : 1);
-					}
-				}
-			}
-			for (int i = 1; i < world.getWidth()-1; i++) 
-			{
-				for (int j = 1; j < world.getHeight()-1; j++) 
-				{
-					for (int k = 1; k < world.getDepth()-1; k++) 
-					{
-						if (drawThis[i][j][k] == 1)
-						{
-							drawThis[i][j][k] = 2;
-							if (drawThis[i+1][j][k] == 0)
-								drawThis[i][j][k] = 1;
-							else if (drawThis[i-1][j][k] == 0)
-								drawThis[i][j][k] = 1;
-							else if (drawThis[i][j+1][k] == 0)
-								drawThis[i][j][k] = 1;
-							else if (drawThis[i][j-1][k] == 0)
-								drawThis[i][j][k] = 1;
-							else if (drawThis[i][j][k+1] == 0)
-								drawThis[i][j][k] = 1;
-							else if (drawThis[i][j][k-1] == 0)
-								drawThis[i][j][k] = 1;
-						}
-					}
-				}
-			}
+			float angle1 = 0;
+			float angle2 = 0;
+			float angle3 = 0;
 			while(!Display.isCloseRequested())
 			{
 				// Clear the screen and depth buffer
 				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 				GL11.glTranslatef(0, -15, -3*world.getDepth());
-				GL11.glRotatef(angle, 0, 1, 0);
-				GL11.glTranslatef(-0.5f*world.getWidth(), 0.5f*world.getHeight(),0);
+				GL11.glRotatef(angle1, 0, 1, 0);
+				GL11.glRotatef(angle2, 1, 0, 0);
+				GL11.glRotatef(angle3, 0, 0, 1);
+				GL11.glTranslatef(-0.5f*world.getWidth(), 0.5f*world.getHeight(),0.5f*world.getDepth());
 
 				for (int i = cuttingDepth; i < world.getWidth() - cuttingDepth; i++) 
 				{
@@ -184,20 +204,42 @@ public class TestStartup3D {
 					GL11.glTranslatef(-i, 0.0f, 0.0f);
 				}
 				Display.update();
-				GL11.glTranslatef(0.5f*world.getWidth(), -0.5f*world.getHeight(),0);
-				GL11.glRotatef(angle, 0, -1, 0);
+				GL11.glTranslatef(0.5f*world.getWidth(), -0.5f*world.getHeight(),-0.5f*world.getDepth());
+				GL11.glRotatef(-angle3, 0, 0, 1);
+				GL11.glRotatef(-angle2, 1, 0, 0);
+				GL11.glRotatef(-angle1, 0, 1, 0);
 				GL11.glTranslatef(0, 15, 3*world.getDepth());
-				angle += 1f;
 				boolean leftPressed   = hasInput(Keyboard.KEY_LEFT);
 				boolean rightPressed   = hasInput(Keyboard.KEY_RIGHT);
-				
+				if (Keyboard.isKeyDown(Keyboard.KEY_A))
+					angle1-= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_D))
+					angle1+= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_W))
+					angle2-= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_S))
+					angle2+= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_Q))
+					angle3-= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_E))
+					angle3+= ROT_SPEED;
+				if (Keyboard.isKeyDown(Keyboard.KEY_R))
+				{
+					angle1 = 0;
+					angle2 = 0;
+					angle3 = 0;
+					load("data/WorleyRules.txt");
+				}
 				if (leftPressed && !pressed) {
 					cuttingDepth++;
+					setDrawingStates();
 					pressed = true;
 				} else if (rightPressed && !pressed) {
 					cuttingDepth--;
 					if (cuttingDepth < 0)
 						cuttingDepth = 0;
+					else
+						setDrawingStates();
 					pressed = true;
 				}
 				
@@ -209,10 +251,6 @@ public class TestStartup3D {
 			Display.destroy();
 		} catch (LWJGLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CriticalFailure e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
